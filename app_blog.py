45ffbd15fcf0a74916f1147f73e3f5872274b395
ihor Vitko <igor.vitko.74@gmail.com>
2021-06-27 20:49:19 +0300

    add func connect_db and create_db, create DB blog.sqlite and table posts, change main page "posts"

--- app_blog.py
+++ app_blog.py
@@ -1,28 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+import time
 
 app = Flask(__name__)
 
 
-@app.route('/')
+def connect_db():
+    connection = sqlite3.connect('blog.sqlite')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def create_db():
+    """
+    This function create DB.
+    Need execute this function once from console
+    :return:
+    New DB if not exit one
+    """
+    db = connect_db()
+    db.cursor().execute(""" CREATE TABLE IF NOT EXISTS posts
+                        (id integer PRIMARY KEY AUTOINCREMENT,
+                        title text NOT NULL,
+                        description text NOT NULL,
+                        date integer
+                         )""")
+    db.commit()
+    db.close()
+
+
+@app.route('/posts')
 def start():
-    pass
+    db = connect_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM posts")
+    posts = cur.fetchall()
+
+    db.close()
+
+    param = {
+        'posts': posts,
+        }
+
+    return render_template('index.html', **param)
 
 
-@app.route('/add')
+@app.route('/posts/add')
 def add_post():
     pass
 
 
-@app.route('/edit')
+@app.route('/posts/edit')
 def edit_post():
     pass
 
 
-@app.route('/delete')
+@app.route('/posts/delete')
 def del_post():
     pass
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
 
