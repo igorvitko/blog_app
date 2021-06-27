3a5e09afb87b9acd911d056b815295582096e850
Ihor Vitko <igor.vitko.74@gmail.com>
2021-06-27 16:32:01 +0300

    add another page and their def

--- app_blog.py
+++ app_blog.py
@@ -2,10 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start():
     pass
 
 
-app.run(debug=True)
+@app.route('/add')
+def add_post():
+    pass
+
+
+@app.route('/edit')
+def edit_post():
+    pass
+
+
+@app.route('/delete')
+def del_post():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
