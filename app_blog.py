06e67f85fab511a1648511f46f80ebfb774c9920
Ihor Vitko <igor.vitko.74@gmail.com>
2021-06-28 16:49:44 +0300

    final minimum functionality
    change URL main page, change doc-strings
    change def post/add - add if (error processing),
    change def post/edit, change def post/delete
    add block try/except in all functions
    add new page 'post/<id>/ and new tetpl

--- app_blog.py
+++ app_blog.py
@@ -2,14 +2,11 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect
 import sqlite3
 
-
-
 app = Flask(__name__)
 
 
 def connect_db():
     """
-
     :return:
     new connection with DB
     """
@@ -36,42 +33,71 @@ def create_db():
     db.close()
 
 
-@app.route('/posts')
+@app.route('/')
 def start():
     """
-    Get all posts from DB and view on page.
+    Get list of the posts from DB and show on main page.
     Sort on id - from new to old
+    A text of post is shot view (only first 100 chr)
+    For full text need click on title of the post
     :return:
     Main page of blog with all posts
     """
-    db = connect_db()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM posts ORDER BY id DESC")
-    posts = cur.fetchall()
+    try:
+        db = connect_db()
+        cur = db.cursor()
+        cur.execute("SELECT * FROM posts ORDER BY id DESC")
+        posts = cur.fetchall()
+        db.close()
 
-    db.close()
-
-    param = {
-        'posts': posts,
+        param = {
+            'posts': posts,
         }
+    except sqlite3.Error as e:
+        db.close()
+        return f"Ошибка чтения базы данных... - <i>{e}</i>"
 
     return render_template('index.html', **param)
 
 
-@app.route('/posts/add')
+@app.route('/post/<int:id>')
+def show_post(id):
+    """
+    This page show full text of choice post
+
+    :param id:
+    The id of a post from DB
+    :return:
+    Title and full text of the post
+    """
+    try:
+        db = connect_db()
+        res = db.cursor().execute("SELECT title, description FROM posts WHERE id=? LIMIT 1", (id, )).fetchone()
+        db.close()
+        if res:
+            title, description = res
+            return render_template('post.html', title=title, description=description)
+    except sqlite3.Error as e:
+        db.close()
+        return f"Ошибка чтения базы данных... - <i>{e}</i>"
+
+
+@app.route('/post/add')
 def add_post():
     """
     Add new post. Method 'GET'
-    Format request - <address>/posts/add?title=<text>&description=<text>
+    Format request - <address>/post/add?title=<text>&description=<text>
     Function has a check for the length of the text (title and description)
     :return:
     Main page with new post
     """
-    db = connect_db()
 
     title = request.args.get("title")
     description = request.args.get("description")
     date = datetime.today().strftime("%d-%m-%Y %H:%M")
+
+    if not title or not description:
+        return redirect('/')
 
     data = ()
 
@@ -79,54 +105,80 @@ def add_post():
         data = (title, description, date)
 
     if data:
-        db.cursor().execute("INSERT INTO posts VALUES (NULL, ?, ?, ?)", data)
+        try:
+            db = connect_db()
+            db.cursor().execute("INSERT INTO posts VALUES (NULL, ?, ?, ?)", data)
+            db.commit()
+            db.close()
+        except sqlite3.Error as e:
+            db.close()
+            return f"Ошибка записи в базу данных... - <i>{e}</i>"
 
-    db.commit()
-    db.close()
-
-    return redirect('/posts')
+    return redirect('/')
 
 
-@app.route('/posts/edit')
+@app.route('/post/edit')
 def edit_post():
     """
     This func edit post with id from request
     New text title and description get from request
-    Format request - <address>/posts/edit?id=<int>&title=<text>&description=<text>
+    Format request - <address>/post/edit?id=<int>&title=<text>&description=<text>
     :return:
     Main page with edited post
     """
-    db = connect_db()
 
     id = request.args.get('id')
     title = request.args.get('title')
     description = request.args.get('description')
 
-    db.cursor().execute("UPDATE posts SET title=?, description=? WHERE id=?", (title, description, id))
+    if not id:
+        return redirect('/')
 
-    db.commit()
-    db.close()
+    try:
+        db = connect_db()
 
-    return redirect('/posts')
+        if title and description:
+            db.cursor().execute("UPDATE posts SET title=?, description=? WHERE id=?", (title, description, id))
+            db.commit()
+            db.close()
+            return redirect('/')
+        elif title:
+            db.cursor().execute("UPDATE posts SET title=? WHERE id=?", (title, id))
+            db.commit()
+            db.close()
+            return redirect('/')
+        elif description:
+            db.cursor().execute("UPDATE posts SET description=? WHERE id=?", (description, id))
+            db.commit()
+            db.close()
+            return redirect('/')
+    except sqlite3.Error as e:
+        db.close()
+        return f"Ошибка записи в базу данных... - <i>{e}</i>"
 
-@app.route('/posts/delete')
+
+@app.route('/post/delete')
 def del_post():
     """
     This func delete post with id from request
-    Format request - <address>/posts/delete?id=<int>
+    Format request - <address>/post/delete?id=<int>
     :return:
     Main page without deleted post
     """
-    db = connect_db()
+
     id = request.args.get('id')
 
     if id:
-        db.cursor().execute("DELETE FROM posts WHERE id = ?", id)
+        try:
+            db = connect_db()
+            db.cursor().execute("DELETE FROM posts WHERE id = ?", (id, ))
+            db.commit()
+            db.close()
+        except sqlite3.Error as e:
+            db.close()
+            return f"Ошибка доступа к базе данных... - <i>{e}</i>"
 
-    db.commit()
-    db.close()
-
-    return redirect('/posts')
+    return redirect('/')
 
 
 if __name__ == '__main__':
